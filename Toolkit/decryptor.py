d93ec6196b41c43093b92ef03c6c09b0ded4bfcf
ThomasSt0rm <zezr0@live.com>
2017-10-26 15:09:16 +0200

    add python3 support

--- Toolkit/decryptor.py
+++ Toolkit/decryptor.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from Crypto.Cipher import AES
 import base64
 from sys import argv
@@ -6,4 +7,4 @@ script, secret_key, encrypted_password = argv
 secret_key = secret_key.rjust(32)
 cipher = AES.new(secret_key,AES.MODE_ECB)
 decoded = cipher.decrypt(base64.b64decode(encrypted_password))
-print decoded.strip()
+print(decoded.strip())
