44cf2e3087deb79b249ac3b7baca63a74f0bf53e
ThomasSt0rm <zezr0@live.com>
2017-10-26 12:22:06 +0200

    add password decryptor and update users table

---
+++ Toolkit/decryptor.py
@@ -0,0 +1,9 @@
+from Crypto.Cipher import AES
+import base64
+from sys import argv
+
+script, secret_key, encrypted_password = argv
+secret_key = secret_key.rjust(32)
+cipher = AES.new(secret_key,AES.MODE_ECB)
+decoded = cipher.decrypt(base64.b64decode(encrypted_password))
+print decoded.strip()
